7ae56c18e1238893236b5d9d2b5e9f9e5418b8a2
cvFortes <ruimiguelfortes@gmail.com>
2024-06-27 12:07:05 -0100

    first lopp

--- .history/datacolletor/app/init_20240625200029.py
+++ .history/datacolletor/app/init_20240625200029.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app() :
    app= Flask(__name__ )
-   app.config['SCRET_KEY']='key_Secretggggggggggggggggggggg'
+   app.config['SCRET_KEY']='key_Secret'
 
    from app.views import views
 
